86d0e208bc34485fbe68ccade1afdccf0936e093
Tom Gurion <nagasaki45@gmail.com>
2017-04-10 17:55:44 +0100

    Improve descriptions in setyp.py

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='MAT-Askbot',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      description='Askbot site for the MAT programme',
+      author='Tom Gurion',
+      author_email='t.gurion@qmul.ac.uk',
      )
