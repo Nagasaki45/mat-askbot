4fa098d82d68019291ffb562df13911b1681d4c9
Tom Gurion <nagasaki45@gmail.com>
2017-04-11 12:00:28 +0100

    Update get_wsgi_application in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -15,8 +15,8 @@ except IOError:
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
 #
 # Below for testing only
